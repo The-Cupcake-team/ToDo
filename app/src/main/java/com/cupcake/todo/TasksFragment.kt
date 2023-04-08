2724fb28364ed46bb9688f3b5c99eb63a2b3698d
Ethaar hussien <ethaar7735@gmail.com>
2023-04-08 17:10:56 +0300

    create fragment tasks

---
+++ app/src/main/java/com/cupcake/todo/TasksFragment.kt
@@ -0,0 +1,19 @@
+package com.cupcake.todo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TasksFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    }
+
+
+
