7ba589cf34cfdd1a9c82687859c5fafba4b83ba7
ali mohamed ali <aliabuhussien371@gmail.com>
2023-04-13 01:17:53 +0200

    create personal task presenter

---
+++ app/src/main/java/com/cupcake/todo/presenter/add_personal_task/AddPersonalTaskPresenter.kt
@@ -0,0 +1,31 @@
+package com.cupcake.todo.presenter.add_personal_task
+
+import com.cupcake.todo.model.network.ApiServiceImpl
+import com.cupcake.todo.model.network.response.AddPersonalTaskResponse
+import com.cupcake.todo.model.network.response.BaseResponse
+import com.cupcake.todo.model.network.util.ApiCallback
+import com.cupcake.todo.ui.fragment.add_task.IAddPersonalTask
+
+class AddPersonalTaskPresenter(
+    private val view:IAddPersonalTask
+
+) : ApiCallback<BaseResponse<AddPersonalTaskResponse>> {
+
+    private val service = ApiServiceImpl()
+
+    fun addPersonalTask(title: String, description: String) {
+        view.showLoading()
+        service.addPersonalTask(title, description, this)
+        view.hideLoading()
+    }
+
+    override fun onSuccess(response: BaseResponse<AddPersonalTaskResponse>) {
+        view.onIAddPersonalTaskSuccess()
+    }
+
+    override fun onFailure(throwable: Throwable, statusCode: Int?, message: String?) {
+       view.onIAddPersonalTaskFailure(throwable.toString())
+    }
+
+
+}
