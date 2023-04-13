22512bf57099a1b4c23abd080ed61f661869b637
Hassan Ayman Ahmed <hassan3ayman@gmail.com>
2023-04-14 00:12:53 +0200

    complete the adapter to the assignee recycler

---
+++ app/src/main/java/com/cupcake/todo/ui/fragment/add_team_task/AssigneeRecyclerAdapter.kt
@@ -0,0 +1,56 @@
+package com.cupcake.todo.ui.fragment.add_team_task
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cupcake.todo.R
+import com.cupcake.todo.databinding.ItemProfileImageBinding
+
+class AssigneeRecyclerAdapter(
+    private val listener: IAssigneeClickListener,
+    private val assignees : List<String>
+): RecyclerView.Adapter<AssigneeRecyclerAdapter.ViewHolder>() {
+
+    var selectedPosition = -1
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_profile_image
+            , parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val assignee = assignees[position]
+        val assigneeChars = assignee.take(2).uppercase()
+            holder.binding.apply {
+                name.text = assignee
+                nameChars.text = assigneeChars
+                if (selectedPosition == position){
+                    correct.visibility = View.VISIBLE
+                }else{
+                    correct.visibility = View.GONE
+                }
+                root.setOnClickListener {
+                    if (selectedPosition != -1){
+                        notifyItemChanged(selectedPosition)
+                    }
+                    selectedPosition = position
+                    correct.visibility = View.VISIBLE
+                    listener.onAssigneeItemClicked(assignee)
+                }
+            }
+    }
+
+    override fun getItemCount() = assignees.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = ItemProfileImageBinding.bind(itemView)
+    }
+
+    interface IAssigneeClickListener{
+        fun onAssigneeItemClicked(assignee: String)
+    }
+}
