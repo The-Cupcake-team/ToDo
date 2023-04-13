df792e88310414884bde8fa646e15da4e0c0fbd3
TarekIdrees <tarekdrias321@gmail.com>
2023-04-13 16:27:02 +0200

    add navigate function

--- app/src/main/java/com/cupcake/todo/ui/base/BaseFragment.kt
+++ app/src/main/java/com/cupcake/todo/ui/base/BaseFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.cupcake.todo.R
+import com.cupcake.todo.util.PrefsUtil
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
     abstract val LOG_TAG: String
@@ -20,10 +22,19 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = bindingInflater(layoutInflater, container!!,false)
+        PrefsUtil.initPrefsUtil(requireContext())
         return binding.root
     }
 
     protected fun log(value: String) {
         Log.v(LOG_TAG, value)
+    }
+
+    protected fun navigateToFragment(fragment: Fragment){
+        requireActivity().supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentContainer,fragment)
+            addToBackStack(fragment.javaClass.name)
+            commit()
+        }
     }
 }
