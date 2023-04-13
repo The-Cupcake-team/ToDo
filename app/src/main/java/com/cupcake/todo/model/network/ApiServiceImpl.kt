5c695e02c52f1733a2d3d2855c29ccce550320ca
ameeramjed <pr.ameer98@gmail.com>
2023-04-13 08:59:02 +0300

    rollback to ApiServiceImpl to back code implement register function

--- app/src/main/java/com/cupcake/todo/model/network/ApiServiceImpl.kt
+++ app/src/main/java/com/cupcake/todo/model/network/ApiServiceImpl.kt
@@ -1,6 +1,6 @@
 package com.cupcake.todo.model.network
 
-
+import com.cupcake.todo.BuildConfig
 import com.cupcake.todo.model.network.response.AddPersonalTaskResponse
 import com.cupcake.todo.model.network.response.BaseResponse
 import com.cupcake.todo.model.network.response.RegisterResponse
@@ -13,27 +13,34 @@ class ApiServiceImpl : ApiService {
 
     private val client = ApiClient()
 
-
-    private companion object {
-        const val USERNAME = "username"
-        const val PASSWORD = "password"
-        const val TEAM_ID = "teamId"
-
-        const val TITLE = "title"
-        const val DESCRIPTION = "description"
-        const val ASSIGNEE = "assignee"
-
-        const val ID = "id"
-        const val STATUS = "status"
-    }
-
     override fun register(
         username: String,
         password: String,
         callback: ApiCallback<BaseResponse<RegisterResponse>>,
     ) {
-        TODO("Not yet implemented")
+        val registerBody = FormBody.Builder()
+            .add(USERNAME, username)
+            .add(PASSWORD, password)
+            .add(TEAM_ID, BuildConfig.TEAM_ID)
+            .build()
+
+        client.postRequest(ApiEndPoint.register, registerBody)
+            .enqueueCall(
+                object : ApiCallback<BaseResponse<RegisterResponse>> {
+                    override fun onSuccess(response: BaseResponse<RegisterResponse>) {
+                        callback.onSuccess(response)
+                    }
+
+                    override fun onFailure(
+                        throwable: Throwable,
+                        statusCode: Int?,
+                        message: String?
+                    ) {
+                        callback.onFailure(throwable, statusCode, message)
+                    }
+                })
     }
+
 
     override fun addPersonalTask(
         title: String,
@@ -56,6 +63,19 @@ class ApiServiceImpl : ApiService {
 
             }
         )
+    }
+
+    private companion object {
+        const val USERNAME = "username"
+        const val PASSWORD = "password"
+        const val TEAM_ID = "teamId"
+
+        const val TITLE = "title"
+        const val DESCRIPTION = "description"
+        const val ASSIGNEE = "assignee"
+
+        const val ID = "id"
+        const val STATUS = "status"
     }
 
 }
