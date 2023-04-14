de993f53d1244dc4b6df9489bf15488a7a3e0cac
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-04-14 23:19:16 +0200

    implement parcelable using parcelable annotation for PersonalTask and TeamTask classes

--- app/src/main/java/com/cupcake/todo/model/network/response/PersonalTask.kt
+++ app/src/main/java/com/cupcake/todo/model/network/response/PersonalTask.kt
@@ -1,9 +1,13 @@
 package com.cupcake.todo.model.network.response
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class PersonalTask(
     val id: String,
     val title: String,
     val description: String,
     val status: Int,
     val creationTime: String
-)
+) : Parcelable
--- app/src/main/java/com/cupcake/todo/model/network/response/TeamTask.kt
+++ app/src/main/java/com/cupcake/todo/model/network/response/TeamTask.kt
@@ -1,5 +1,9 @@
 package com.cupcake.todo.model.network.response
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class TeamTask(
     val id: String,
     val title: String,
@@ -7,4 +11,4 @@ data class TeamTask(
     val assignee: String,
     val status: Int,
     val creationTime: String
-)
+) : Parcelable
