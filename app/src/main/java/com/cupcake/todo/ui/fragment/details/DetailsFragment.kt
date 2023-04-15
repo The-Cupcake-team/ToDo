2180e234552d0058a8c262218e82c63062501d28
Mustafa Sabe Alarab <mustafa777alarab@gmail.com>
2023-04-15 11:10:15 +0200

    more organize code

--- app/src/main/java/com/cupcake/todo/ui/fragment/details/DetailsFragment.kt
+++ app/src/main/java/com/cupcake/todo/ui/fragment/details/DetailsFragment.kt
@@ -13,11 +13,11 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.cupcake.todo.R
 import com.cupcake.todo.databinding.FragmentDetailsBinding
-import com.cupcake.todo.presenter.details.DetailsPresenter
-import com.cupcake.todo.presenter.model.PersonalTask
-import com.cupcake.todo.presenter.model.Task
-import com.cupcake.todo.presenter.model.TeamTask
+import com.cupcake.todo.ui.fragment.personal_tasks.model.PersonalTask
+import com.cupcake.todo.ui.fragment.personal_tasks.model.Task
+import com.cupcake.todo.ui.fragment.personal_tasks.model.TeamTask
 import com.cupcake.todo.ui.base.BaseFragment
+import com.cupcake.todo.ui.fragment.details.adapter.DetailsAdapter
 
 class DetailsFragment : BaseFragment<FragmentDetailsBinding>(), IDetailsView {
 
@@ -31,6 +31,7 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>(), IDetailsView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         presenter = DetailsPresenter(this)
+
         binding.textViewDetails.movementMethod = ScrollingMovementMethod()
 
         task = (arguments?.getParcelable(TASK_DETAILS) as Task?)!!
@@ -46,22 +47,26 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>(), IDetailsView {
             }
         }
 
-
-        initSpinner()
-
         binding.apply {
             textViewTitle.text = task.title
             textViewDetails.text = task.description
             textViewDate.text = task.creationTime
         }
 
+        initSpinner()
     }
 
     override fun showLoading() {
+        requireActivity().runOnUiThread {
+            binding.progressBarLoading.visibility = View.VISIBLE
+        }
         Log.v(LOG_TAG, "showLoading")
     }
 
     override fun hideLoading() {
+        requireActivity().runOnUiThread {
+            binding.progressBarLoading.visibility = View.GONE
+        }
         Log.v(LOG_TAG, "hideLoading")
     }
 
@@ -75,36 +80,33 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>(), IDetailsView {
 
     private fun initSpinner() {
 
-        val mAdapter =
-            ArrayAdapter(requireContext(), R.layout.drop_down_list_item, items)
-
-        binding.dropdownMenu.setText(items[task.status])
+        val mAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_list_item, items)
 
         binding.dropdownMenu.apply {
+
+            setText(items[task.status])
+
             setAdapter(mAdapter)
+
             showDropdown(mAdapter)
 
-            log(task.toString())
-
-
-            onItemClickListener =
-                AdapterView.OnItemClickListener { _, _, position, _ ->
-                    when (task) {
-                        is TeamTask -> {
-                            task.id?.let { presenter.detailsUpDate(it, position, false) }
-                            log("change team task to $position")
-                        }
-                        is PersonalTask -> {
-                            task.id?.let { presenter.detailsUpDate(it, position, true) }
-                            log("change personal task to $position")
-                        }
+            onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                when (task) {
+                    is TeamTask -> {
+                        task.id?.let { presenter.detailsUpDate(it, position, false) }
+                        log("change team task to $position")
+                    }
+                    is PersonalTask -> {
+                        task.id?.let { presenter.detailsUpDate(it, position, true) }
+                        log("change personal task to $position")
                     }
                 }
+            }
         }
     }
 
     private fun AutoCompleteTextView.showDropdown(adapter: ArrayAdapter<String>?) {
-        if(!TextUtils.isEmpty(this.text.toString())){
+        if (!TextUtils.isEmpty(this.text.toString())) {
             adapter?.filter?.filter(null)
         }
     }
