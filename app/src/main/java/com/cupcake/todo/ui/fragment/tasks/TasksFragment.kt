97bce4fd97874a8959bedbea3ef64052cf3085f0
ameeramjed <pr.ameer98@gmail.com>
2023-04-17 17:58:47 +0300

    implement tabs with viewpager in task fragment

--- app/src/main/java/com/cupcake/todo/ui/fragment/tasks/TasksFragment.kt
+++ app/src/main/java/com/cupcake/todo/ui/fragment/tasks/TasksFragment.kt
@@ -1,16 +1,44 @@
 package com.cupcake.todo.ui.fragment.tasks
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.cupcake.todo.R
 import com.cupcake.todo.databinding.FragmentTasksBinding
 import com.cupcake.todo.ui.base.BaseFragment
+import com.cupcake.todo.ui.fragment.personal_tasks.PersonalTasksFragment
+import com.cupcake.todo.ui.fragment.tasks.adapter.ViewPagerTasksAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class TasksFragment : BaseFragment<FragmentTasksBinding>() {
     override val LOG_TAG: String = this::class.java.name
     override val bindingInflater: (LayoutInflater, ViewGroup, Boolean) -> FragmentTasksBinding
         get() = FragmentTasksBinding::inflate
+
+    private val fragmentTasks = mapOf(
+        0 to PersonalTasksFragment(),
+        1 to PersonalTasksFragment(),
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val titleTabTasks = resources.getStringArray(R.array.task)
+        val adapter = ViewPagerTasksAdapter(
+            fragmentManager = requireActivity().supportFragmentManager,
+            fragmentItems = fragmentTasks,
+            lifecycle = lifecycle,
+        )
+        binding.apply {
+            viewPagerTasks.adapter = adapter
+            TabLayoutMediator(tabTasks, viewPagerTasks) { tab, position ->
+                tab.text = titleTabTasks[position]
+            }.attach()
+        }
+    }
 }
+
+
 
 
 
