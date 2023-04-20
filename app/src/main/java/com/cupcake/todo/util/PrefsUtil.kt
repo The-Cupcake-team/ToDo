6c2253be991bc95578301f0a388ae494eb1ee55e
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-04-20 05:01:40 +0200

    fix home screen design

--- app/src/main/java/com/cupcake/todo/util/PrefsUtil.kt
+++ app/src/main/java/com/cupcake/todo/util/PrefsUtil.kt
@@ -7,7 +7,7 @@ object PrefsUtil {
     private var todoSharedPreferences: SharedPreferences? = null
     private var todoSharedPreferencesEditor: SharedPreferences.Editor? = null
     private const val SHARED_PREFERENCES = "TodoSharedPreferences"
-    private const val TOKEN = "token"
+    private const val TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6ImFjZTIyY2RkLTk5YjAtNDQxYS1hMGJjLWMwMGE4NTVmNTZmMyIsInRlYW1JZCI6IjkwOTJlZDcwLTMxNTMtNDEwNi1iYTE4LTYxYTU3Yjk0NmI2NCIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgyMTMwODQyfQ.5n30PjeWyER38hYpYtkEf03iu7IHi6GzUhMSjo5bTjw"
 
     fun initPrefsUtil(context: Context){
         todoSharedPreferences = context.getSharedPreferences(SHARED_PREFERENCES,Context.MODE_PRIVATE)
